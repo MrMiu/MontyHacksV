95d32a0ae174a732745197c8069b87e6c1b997b6
Aahil Shaikh <aahil.aref@gmail.com>
2022-06-11 14:45:17 -0400

    Got selection working in app

---
+++ flutter_app/lib/models/county.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class County {
+  
+  final String name;
+  final Color color;
+
+  County({required this.name, required this.color});
+}
--- flutter_app/lib/services/api.dart
+++ flutter_app/lib/services/api.dart
@@ -39,4 +39,6 @@ class Api {
     print(data);
     return data;
   }
+
+  
 }
